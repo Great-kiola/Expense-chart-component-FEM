d8e215f9703ff33ce9b8ba870968a40954684911
Great-kiola <hollabodey.kilaso@gmail.com>
2023-08-01 22:45:46 +0100

    Made changes to the index and html files

--- script.js
+++ script.js
@@ -1,49 +1,48 @@
 // console.log("Hello world!");
 
-const ctx = document.getElementById("myChart");
 
-// let Chart;
+let ctx = document.getElementById("myChart").getContext('2d');
 
-new Chart(ctx, {
+let myChart = new Chart(ctx, {
   type: "line",
   data: {
     labels: ["Red", "Blue", "Yellow", "Green", "Purple"],
+
     datasets: [
       {
         label: "# of Votes",
         data: [12, 11, 5, 2, 13],
         tension: 0.3,
-        // borderColor: '#00ba34',
-        // borderWidth: 3,
-        // radius: 3,
-        // hitRadius: 10,
-        // hoverRadius: 10,
-        // backgroundColor: '#000'
+        borderColor: '#306B34',
+        borderWidth: 2
       },
 
       {
         label: "# of visitors",
         data: [10, 11, 2, 3, 5, 7],
         tension: 0.3,
+        borderColor: '#D8973C',
+        borderWidth: 2
       }
-    ],
+    ]
   },
 
 
   options: {
     plugins: {
         legend: {
-            display: true,
+            display: false,
             padding: {
                 right: 10,
                 left: 200
             }
         }
     },
+
     scales: {
       y: {
         beginAtZero: true,
-      },
+      }
     },
   },
 });
