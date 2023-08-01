f063b21ca5fe8f3213892086f0ea13273e06f248
Great-kiola <hollabodey.kilaso@gmail.com>
2023-08-01 15:52:31 +0100

    Added a new dataset

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 // console.log("Hello world!");
 
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById("myChart");
 
 // let Chart;
 
 new Chart(ctx, {
-  type: 'line',
+  type: "line",
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 3
-    }]
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 11, 5, 2, 13],
+        tension: 0.3,
+        // borderColor: '#00ba34',
+        // borderWidth: 3,
+        // radius: 3,
+        // hitRadius: 10,
+        // hoverRadius: 10,
+        // backgroundColor: '#000'
+      },
+
+      {
+        label: "# of visitors",
+        data: [10, 11, 2, 3, 5, 7],
+        tension: 0.3,
+      }
+    ],
   },
+
+
   options: {
+    plugins: {
+        legend: {
+            display: true,
+            padding: {
+                right: 10,
+                left: 200
+            }
+        }
+    },
     scales: {
       y: {
-        beginAtZero: true
-      }
-    }
-  }
+        beginAtZero: true,
+      },
+    },
+  },
 });
