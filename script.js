2e431d990fcb72a9cd08be745ae5d678c5c902d1
Great-kiola <hollabodey.kilaso@gmail.com>
2023-08-02 13:16:19 +0100

    Made changes to the html, css and js files

--- script.js
+++ script.js
@@ -1,39 +1,86 @@
 // console.log("Hello world!");
+let ctx = document.getElementById("myChart");
+
+async function fetchData() {
+  const url = "./assets/data.json";
+  const response = await fetch(url);
+
+  const dataPoints = await response.json();
+
+  // console.log(dataPoints);
 
 
-let ctx = document.getElementById("myChart").getContext('2d');
+  return dataPoints;
+}
 
-let myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple"],
+fetchData().then((dataPoints) => {
 
-    datasets: [
-      {
-        data: [12, 11, 5, 2, 13],
-        tension: 0.3,
-        borderRadius: 5,
-        backgroundColor: 'hsl(10, 79%, 65%)'
-      },
-    ]
-  },
+  // console.log(dataPoints[6].amount);
 
 
-  options: {
-    plugins: {
+  const month = dataPoints.map(function (index) {
+    return index.day;
+  });
+
+  const amount = dataPoints.map(function (index) {
+    return index.amount;
+  });
+
+
+  Chart.defaults.font.family = "DM sans";
+  Chart.defaults.font.size = 12;
+
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: month,
+
+      datasets: [
+        {
+          data: amount,
+          tension: 0.3,
+          borderRadius: 5,
+          backgroundColor: "hsl(10, 79%, 65%)",
+          cursor: "pointer",
+          hoverBackgroundColor: "hsl(186, 34%, 60%)",
+        },
+      ],
+    },
+
+    options: {
+      plugins: {
         legend: {
-            display: false,
-            padding: {
-                right: 10,
-                left: 200
-            }
-        }
-    },
+          display: false,
+          padding: {
+            right: 10,
+            left: 200,
+          },
+        },
+      },
 
-    scales: {
-      y: {
-        beginAtZero: true,
-      }
+      scales: {
+        y: {
+          beginAtZero: true,
+
+          ticks: {
+            display: false,
+          },
+
+          grid: {
+            display: false,
+            drawTicks: false,
+          },
+        },
+
+        x: {
+          grid: {
+            display: false,
+            drawTicks: false,
+          },
+        },
+      },
     },
-  },
+  });
 });
+
+// fetchData();
